e978110f43bfcea995d83bd0fd0e42b2492cae6c
rabbil <rabbil@nomadtech.com.bd>
2020-01-29 19:19:45 +0600

    :fire:

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component'
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: 'Frankestein'
-        },
-        {
-          id: 2,
-          name: 'Instein'
-        },
-        {
-          id: 3,
-          name: 'Frank'
-        },
-      ]
+      monsters: []
     }
   }
-
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()
+      .then(users => this.setState({monsters:users})))
+  }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
+        <CardList name="jason"/>
+        {this.state.monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
-          ))
-        }
+        ))}
       </div>
     );
   }
 }
-
 
 export default App;
