78f2a8e0f7382441d85a6a4ca3f6e814a010c357
rabbil <rabbil@nomadtech.com.bd>
2020-02-03 18:51:13 +0600

    3,26

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component'
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends React.Component { 
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
   componentDidMount() {
@@ -18,9 +20,19 @@ class App extends React.Component {
       .catch(err => console.log(err))
   }
   render() {
+
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} /> 
+        <SearchBox 
+        placeholder="search monster" 
+        handleChange={(e) => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters} /> 
       </div>
     );
   }
